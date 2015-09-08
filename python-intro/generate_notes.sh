e8429fe24b2d75622e5c94970ee287482fe9c046
Jongbin Jung <jongbin@stanford.edu>
2015-09-08 12:53:57 -0700

    clean up files

--- python-intro/generate_notes.sh
+++
@@ -1,3 +0,0 @@
-ipython nbconvert --to pdf 01-meet-python.ipynb --template notes
-ipython nbconvert --to pdf 02-python-programming.ipynb --template notes
-ipython nbconvert --to pdf 03-jupyter-pandas-and-more.ipynb --template notes
